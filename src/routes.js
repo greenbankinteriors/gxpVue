e5a61197c471abbac6c0ead0abc62cffc4add135
Adam Jones <adam.jones.91@hotmail.com>
2017-10-26 14:12:08 +0100

    GXP - updated routes after removing test files

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,11 @@ import buttons from './pages/buttons';
 import inputs from './pages/inputs';
 import notifications from './pages/notifications';
 import stepIndicator from './pages/step-indicator';
-import Test from './components/test';
-import Users from './components/users';
 
 export default [
     { path: '/', component: Home },
     { path: '/components/buttons', component: buttons },
     { path: '/components/inputs', component: inputs },
     { path: '/components/notifications', component: notifications },
-    { path: '/components/step-indicator', component: stepIndicator },
-    { path: '/test', component: Test }
+    { path: '/components/step-indicator', component: stepIndicator }
 ]
